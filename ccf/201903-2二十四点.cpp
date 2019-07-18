939293815c4ba8a1453214633a581452fd1d49b1
bleuxr <38353817+bleuxr@users.noreply.github.com>
2019-07-18 10:35:13 +0800

    complete 201903-2

--- ccf/201903-2二十四点.cpp
+++ ccf/201903-2二十四点.cpp
@@ -1,12 +1,50 @@
 #include<iostream>
 #include<stack>
+#include<string>
 using namespace std;
 
 int main(){
-    int n;
+    int n,p,q;
+    string s;
+    stack<int> num;
+    stack<char> op;
     scanf("%d",&n);
     while(n--){
-
+        cin>>s;
+        for(int i=0;i<7;i++){
+            if(s[i]>'0'&&s[i]<='9'){
+                num.push(s[i]-'0');
+            }
+            else if(s[i]=='+'){
+                op.push('+');
+            }
+            else if(s[i]=='-'){
+                i++;
+                num.push((-1)*(s[i]-'0'));
+                op.push('+');
+            }
+            else if(s[i]=='x'){
+                i++;
+                p=num.top();
+                num.pop();
+                num.push(p*(s[i]-'0'));
+            }
+            else if(s[i]=='/'){
+                i++;
+                p=num.top();
+                num.pop();
+                num.push(p/(s[i]-'0'));
+            }
+        }
+        while(!op.empty()){
+            q=num.top();
+            num.pop();
+            p=num.top();
+            num.pop();
+            op.pop();
+            num.push(q+p);
+        }
+        cout<<(num.top()==24?"Yes":"No")<<endl;
     }
     return 0;
 }
